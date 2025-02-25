2552dec747ae3175600d7834173d59b503feb8f8
Gentil Teixeira <gentilto@lsretail.com>
2025-02-25 16:08:19 +0000

    Notifications

---
+++ VuetifyPlayground/Notifications/NotificationSolutionsExample.vue
@@ -0,0 +1,381 @@
+<template>
+  <v-container class="d-flex justify-center">
+    <v-alert
+      color="grey darken-1"
+      variant="tonal"
+      class="rounded-lg custom-alert"
+    >
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <!-- Title and dismiss button -->
+        <v-col cols="8" class="pl-2">
+          <span class="text-body-3 success" style="font-weight: 600"
+            >Solution created</span
+          >
+        </v-col>
+        <v-col cols="2" class="text-right">
+          <v-btn icon size="x-small" variant="text" density="comfortable">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row align="center" no-gutters style="height: 50px">
+        <!-- Azure icon -->
+        <v-col align="left" cols="2">
+          <v-badge color="transparent" location="bottom end" class="badge">
+            <template #badge>
+              <v-icon size="18" color="success">mdi-check-circle</v-icon>
+            </template>
+            <v-avatar>
+              <v-icon size="50">mdi-microsoft-azure</v-icon>
+            </v-avatar>
+          </v-badge>
+        </v-col>
+        <!-- Description and buttons -->
+        <v-col cols="9" class="pl-2">
+          <div style="font-size: small">
+            GentilTo's Management Module was created.
+          </div>
+          <div style="font-size: small"></div>
+          <div style="font-size: small"></div>
+        </v-col>
+        <v-col cols="1" />
+      </v-row>
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <v-col cols="9" class="pl-2">
+          <v-btn variant="outlined" size="x-small" rounded class="mr-1"
+            >App Service</v-btn
+          >
+          <v-btn variant="outlined" size="x-small" rounded class="mr-1"
+            >Solution</v-btn
+          >
+        </v-col>
+        <!-- Timestamp -->
+        <v-col cols="1" class="text-center">
+          <small>1d</small>
+        </v-col>
+      </v-row>
+    </v-alert>
+
+    <span class="mr-2"></span>
+    <v-alert
+      color="grey darken-1"
+      variant="tonal"
+      class="rounded-lg custom-alert"
+    >
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <!-- Title and dismiss button -->
+        <v-col cols="9" class="pl-2">
+          <span class="text-body-3" style="font-weight: 600"
+            >Solution created</span
+          >
+        </v-col>
+        <v-col cols="1" class="text-center">
+          <v-btn icon size="x-small" variant="text" density="comfortable">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row align="center" no-gutters style="height: 50px">
+        <!-- Azure icon -->
+        <v-col align="left" cols="2">
+          <v-badge color="transparent" location="bottom end" class="badge">
+            <template #badge>
+              <v-icon size="18" color="success">mdi-check</v-icon>
+            </template>
+            <v-avatar>
+              <v-icon size="50">mdi-docker</v-icon>
+            </v-avatar>
+          </v-badge>
+        </v-col>
+        <!-- Description and buttons -->
+        <v-col cols="9" class="pl-2">
+          <div style="font-size: small">
+            GentilTo's Management Module was created.
+          </div>
+          <div style="font-size: small"></div>
+          <div style="font-size: small"></div>
+        </v-col>
+        <v-col cols="1" />
+      </v-row>
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <v-col cols="9" class="pl-2">
+          <v-btn variant="outlined" size="x-small" rounded class="mr-1"
+            >App Service</v-btn
+          >
+          <v-btn variant="outlined" size="x-small" rounded class="mr-1"
+            >Solution</v-btn
+          >
+        </v-col>
+        <!-- Timestamp -->
+        <v-col cols="1" class="text-center">
+          <small>1d</small>
+        </v-col>
+      </v-row>
+    </v-alert>
+  </v-container>
+
+  <v-container class="d-flex justify-center">
+    <v-alert
+      color="grey darken-1"
+      variant="tonal"
+      class="rounded-lg custom-alert"
+    >
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <!-- Title and dismiss button -->
+        <v-col cols="8" class="pl-2">
+          <span class="text-body-3" style="font-weight: 600; color: dodgerblue"
+            >Solution updated</span
+          >
+        </v-col>
+        <v-col cols="2" class="text-right">
+          <v-btn icon size="x-small" variant="text" density="comfortable">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row align="center" no-gutters style="height: 50px">
+        <!-- Azure icon -->
+        <v-col align="left" cols="2">
+          <v-badge color="transparent" location="bottom end" class="badge">
+            <template #badge>
+              <v-icon size="18" color="info">mdi-arrow-up-circle</v-icon>
+            </template>
+            <v-avatar>
+              <v-icon size="50">mdi-microsoft-azure</v-icon>
+            </v-avatar>
+          </v-badge>
+        </v-col>
+        <!-- Description and buttons -->
+        <v-col cols="9" class="pl-2">
+          <div style="font-size: small">
+            GentilTo's Management Module was updated.
+          </div>
+          <div style="font-size: small"></div>
+          <div style="font-size: small"></div>
+        </v-col>
+        <v-col cols="1" />
+      </v-row>
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <v-col cols="9" class="pl-2">
+          <v-btn variant="outlined" size="x-small" rounded class="mr-1"
+            >App Service</v-btn
+          >
+          <v-btn variant="outlined" size="x-small" rounded class="mr-1"
+            >Solution</v-btn
+          >
+        </v-col>
+        <!-- Timestamp -->
+        <v-col cols="1" class="text-center">
+          <small>1d</small>
+        </v-col>
+      </v-row>
+    </v-alert>
+
+    <span class="mr-2"></span>
+    <v-alert
+      color="grey darken-1"
+      variant="tonal"
+      class="rounded-lg custom-alert"
+    >
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <!-- Title and dismiss button -->
+        <v-col cols="9" class="pl-2">
+          <span class="text-body-3" style="font-weight: 600"
+            >Solution updated</span
+          >
+        </v-col>
+        <v-col cols="1" class="text-center">
+          <v-btn icon size="x-small" variant="text" density="comfortable">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row align="center" no-gutters style="height: 50px">
+        <!-- Azure icon -->
+        <v-col align="left" cols="2">
+          <v-badge color="transparent" location="bottom end" class="badge">
+            <template #badge>
+              <v-icon size="15" color="info" style="font-size: large"
+                >mdi-arrow-up</v-icon
+              >
+            </template>
+            <v-avatar>
+              <v-icon size="50">mdi-docker</v-icon>
+            </v-avatar>
+          </v-badge>
+        </v-col>
+        <!-- Description and buttons -->
+        <v-col cols="9" class="pl-2">
+          <div style="font-size: small">
+            GentilTo's Management Module was updated.
+          </div>
+          <div style="font-size: small"></div>
+          <div style="font-size: small"></div>
+        </v-col>
+        <v-col cols="1" />
+      </v-row>
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <v-col cols="9" class="pl-2">
+          <v-btn variant="outlined" size="x-small" rounded class="mr-1"
+            >App Service</v-btn
+          >
+          <v-btn variant="outlined" size="x-small" rounded class="mr-1"
+            >Solution</v-btn
+          >
+        </v-col>
+        <!-- Timestamp -->
+        <v-col cols="1" class="text-center">
+          <small>1d</small>
+        </v-col>
+      </v-row>
+    </v-alert>
+  </v-container>
+
+  <v-container class="d-flex justify-center">
+    <v-alert
+      color="grey darken-1"
+      variant="tonal"
+      class="rounded-lg custom-alert"
+    >
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <!-- Title and dismiss button -->
+        <v-col cols="9" class="pl-2">
+          <span class="text-body-1 error" style="font-weight: 600"
+            >Solution deleted</span
+          >
+        </v-col>
+        <v-col cols="1" class="text-center">
+          <v-btn icon size="x-small" variant="text" density="comfortable">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row align="center" no-gutters style="height: 50px">
+        <!-- Azure icon -->
+        <v-col align="left" cols="2">
+          <v-badge color="transparent" location="bottom end" class="badge">
+            <template #badge>
+              <v-icon size="18" color="error">mdi-minus-circle</v-icon>
+            </template>
+            <v-avatar>
+              <v-icon size="50">mdi-microsoft-azure</v-icon>
+            </v-avatar>
+          </v-badge>
+        </v-col>
+        <!-- Description and buttons -->
+        <v-col cols="9" class="pl-2">
+          <div style="font-size: small">
+            GentilTo's Management Module was deleted.
+          </div>
+          <div style="font-size: x-small"></div>
+          <div style="font-size: x-small"></div>
+        </v-col>
+        <v-col cols="1" />
+      </v-row>
+      <v-row align="center" no-gutters>
+        <v-col cols="3" />
+        <v-col cols="8"> </v-col>
+        <!-- Timestamp -->
+        <v-col cols="1" class="text-center">
+          <small>1d</small>
+        </v-col>
+      </v-row>
+    </v-alert>
+
+    <span class="mr-2"></span>
+
+    <v-alert
+      color="grey darken-1"
+      variant="tonal"
+      class="rounded-lg custom-alert"
+    >
+      <v-row align="center" no-gutters>
+        <v-col cols="2" />
+        <!-- Title and dismiss button -->
+        <v-col cols="9" class="pl-2">
+          <span class="text-body-1" style="font-weight: 600"
+            >Solution deleted</span
+          >
+        </v-col>
+        <v-col cols="1" class="text-center">
+          <v-btn icon size="x-small" variant="text" density="comfortable">
+            <v-icon>mdi-close-</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row align="center" no-gutters style="height: 50px">
+        <!-- Azure icon -->
+        <v-col align="left" cols="2">
+          <v-badge color="transparent" location="bottom end" class="badge">
+            <template #badge>
+              <v-icon size="12" color="error" style="font-size: large"
+                >mdi-minus</v-icon
+              >
+            </template>
+            <v-avatar>
+              <v-icon size="50">mdi-docker</v-icon>
+            </v-avatar>
+          </v-badge>
+        </v-col>
+        <!-- Description and buttons -->
+        <v-col cols="9" class="pl-2">
+          <div style="font-size: small">
+            GentilTo's Management Module was deleted.
+          </div>
+          <div style="font-size: x-small"></div>
+          <div style="font-size: x-small"></div>
+        </v-col>
+        <v-col cols="1" />
+      </v-row>
+      <v-row align="center" no-gutters>
+        <v-col cols="3" />
+        <v-col cols="8"> </v-col>
+        <!-- Timestamp -->
+        <v-col cols="1" class="text-center">
+          <small>1d</small>
+        </v-col>
+      </v-row>
+    </v-alert>
+  </v-container>
+</template>
+
+<style scoped>
+  .custom-alert {
+    max-width: 380px;
+    max-height: 140px;
+  }
+  .light-theme {
+    background-color: #424242;
+  }
+  .dark-theme {
+    background-color: #505050;
+    color: white;
+  }
+  .success {
+    color: #02a481;
+  }
+  .cancel {
+    color: grey;
+  }
+  .error {
+    color: #c43633;
+  }
+  .info {
+    color: ;
+  }
+  * {
+    font-family: 'Segoe UI';
+  }
+  .primary {
+    color: #5c4582;
+  }
+</style>
